f064c3d57d588399302d3e2b512a545aa2f1c1a7
Udisha <135735575+udisha0530@users.noreply.github.com>
2024-11-11 16:51:52 +0530

    Update seed.sql

--- database/postgresql/seed.sql
+++ database/postgresql/seed.sql
@@ -48,7 +48,8 @@ DECLARE
 BEGIN
     FOR record IN SELECT symbol, ffm_cap FROM public.stock_data LOOP
         INSERT INTO public.trade_info (
-            symbol,
+            trade_id
+            instrument_id,
             traded_volume_lakhs,
             traded_value_cr,
             total_market_cap_cr,
@@ -59,6 +60,7 @@ BEGIN
             face_value
         )
         VALUES (
+            generate_trade_id()
             record.symbol,
             ROUND((1 + (random() * 100))::numeric, 2),
             ROUND((10 + (random() * 500))::numeric, 2),
@@ -72,14 +74,23 @@ BEGIN
     END LOOP;
 END $$;
 
--- Insert data into the price_info table
+-- Insert data into the instrument table
 DO $$
 DECLARE
     stock_record RECORD;
+    info_record  RECORD;
 BEGIN
-    FOR stock_record IN SELECT symbol, nm_52w_h, nm_52w_l FROM public.stock_data LOOP
-        INSERT INTO public.price_info (
-            symbol,
+   FOR stock_record IN 
+        SELECT symbol, week_52_high, week_52_low 
+        FROM public.stock_data
+    LOOP
+        -- Fetch the corresponding data from instrument_info for the current symbol
+        SELECT long_name, industry, stock_exchange
+        INTO info_record
+        FROM public.instrument_info
+        WHERE instrument_info_id = stock_record.symbol;
+        INSERT INTO public.instrument (
+            instrument,
             week_52_high,
             week_52_low,
             upper_band,
@@ -87,7 +98,13 @@ BEGIN
             price_band,
             daily_volatility,
             annualised_volatility,
-            tick_size
+            tick_size,
+            long_name,
+            industry,
+            stock_exchange,
+            pe_ratio,
+            dividend_yield,
+            roe
         ) VALUES (
             stock_record.symbol,
             stock_record.week_52_high,
@@ -97,7 +114,14 @@ BEGIN
             'No Band',  
             ROUND((1 + random() * 5) :: NUMERIC, 2),  -- Random daily volatility between 1 and 5
             ROUND((10 + random() * 25) :: NUMERIC, 2),  -- Random annualised volatility between 10 and 35
-            ROUND((0.05 + random() * 0.15) :: NUMERIC, 2)  -- Random tick size between 0.05 and 0.2
+            ROUND((0.05 + random() * 0.15) :: NUMERIC, 2),-- Random tick size between 0.05 and 0.2
+            info_record.long_name,
+            info_record.industry,
+            info_record.stock_exchange,
+            ROUND((random() * (50 - 5) + 5)::numeric, 2);
+            ROUND((random() * 10)::numeric, 2);
+            ROUND((random() * (25 - 5) + 5)::numeric, 2);
+            
         );
     END LOOP;
 END $$;
