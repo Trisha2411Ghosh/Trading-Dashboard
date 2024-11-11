e553b210ebc5fe1e62e8e9979df56be8b11506ef
Udisha <135735575+udisha0530@users.noreply.github.com>
2024-11-11 22:58:08 +0530

    Update schema.sql

--- database/Clickhouse/schema.sql
+++ database/Clickhouse/schema.sql
@@ -17,7 +17,8 @@ CREATE TABLE IF NOT EXISTS stock_data (
 ORDER BY symbol;
 
 CREATE TABLE IF NOT EXISTS trade_info (
-    symbol String,
+    trade_id String,
+    instrument_id,
     traded_volume_lakhs Decimal(10, 2),
     traded_value_cr Decimal(10, 2),
     total_market_cap_cr Decimal(10, 2),
@@ -29,15 +30,21 @@ CREATE TABLE IF NOT EXISTS trade_info (
 ) ENGINE = MergeTree() 
 ORDER BY symbol;
 
-CREATE TABLE IF NOT EXISTS price_info (
-    symbol String,
+CREATE TABLE IF NOT EXISTS instrument (
+    instrument_id String,
     week_52_high Decimal(10, 2),
     week_52_low Decimal(10, 2),
-    upper_band Decimal(10, 2),
-    lower_band Decimal(10, 2),
+    upper_band Decimal(38, 2),
+    lower_band Decimal(38, 2),
     price_band String,
-    daily_volatility Decimal(5, 2),
-    annualised_volatility Decimal(5, 2),
-    tick_size Decimal(5, 2)
+    daily_volatility Decimal(38, 2),
+    annualised_volatility Decimal(38, 2),
+    tick_size Decimal(38, 2),
+    long_name String,
+    industry String,
+    stock_exchange String,
+    pe_ratio Decimal(10, 2),
+    dividend_yield Decimal(5, 2),
+    roe Decimal(5, 2)
 ) ENGINE = MergeTree() 
 ORDER BY symbol;
