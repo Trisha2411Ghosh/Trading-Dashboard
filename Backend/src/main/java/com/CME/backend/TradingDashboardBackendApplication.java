bd8151f3be3496ae23f1da63ae6ed9c987141f8f
smritisinha24 <smritisinha2003@gmail.com>
2024-11-20 16:07:44 +0530

    adding instrument id in combined data

--- Backend/src/main/java/com/CME/backend/TradingDashboardBackendApplication.java
+++ Backend/src/main/java/com/CME/backend/TradingDashboardBackendApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 
 
-public class BackendApplication {
+public class TradingDashboardBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(TradingDashboardBackendApplication.class, args);
 	}
 }
