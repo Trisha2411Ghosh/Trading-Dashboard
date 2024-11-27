3e2690d78844c0368289ce3b2ff7a3ceac2ee0bf
Udisha <135735575+udisha0530@users.noreply.github.com>
2024-11-28 02:58:48 +0530

    Update aggregate_query.sql

--- database/aggregate_query.sql
+++ database/aggregate_query.sql
@@ -1,18 +1,19 @@
-            SELECT
-                instrument_id,
-                trade_date,
-                AVG(traded_value_cr) AS avg_price,
-                SUM(traded_volume_lakhs) AS total_volume,
-                MAX(traded_value_cr) AS max_price
-            FROM
-                trade_info
-            WHERE
-                trade_date BETWEEN ? AND ?
-            GROUP BY
-                instrument_id, trade_date
-            ORDER BY
-                trade_date ASC, instrument_id ASC
-            LIMIT 100
+SELECT
+    instrument_id,
+    trade_date,
+    AVG(traded_value_cr) AS avg_price,
+    SUM(traded_volume_lakhs) AS total_volume,
+    MAX(traded_value_cr) AS max_price
+FROM
+    trade_info
+WHERE
+    trade_date BETWEEN ? AND ?
+GROUP BY
+    instrument_id, trade_date
+ORDER BY
+    trade_date ASC, instrument_id ASC
+LIMIT 100
+            
 SELECT
             industry,
             toStartOfMonth(trade_date) AS trade_month,
